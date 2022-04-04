f2cf81791a511573b99e8bfcd2b83ee54b1cb596
Benjamin Cooke Williams <83699317+bcookew@users.noreply.github.com>
2022-04-03 21:43:31 -0400

    Boxing Unboxing Assignment Complete

---
+++ BoxingUnboxing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<object> box = new List<object>();
+            box.Add(7);
+            box.Add(28);
+            box.Add(-1);
+            box.Add(true);
+            box.Add("chair");
+            int sum = 0;
+            foreach (var item in box)
+            {
+                System.Console.WriteLine(item);
+                if (item is int)
+                {
+                    sum += (int)item;
+                }
+            }
+            System.Console.WriteLine(sum);
+
+        }
+    }
+}
