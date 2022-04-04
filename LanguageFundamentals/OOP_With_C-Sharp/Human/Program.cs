d8afa71672bd6af4568402f0dbe4d642d2c4d5e8
Benjamin Cooke Williams <83699317+bcookew@users.noreply.github.com>
2022-04-04 12:27:39 -0400

    Human Complete

--- LanguageFundamentals/OOP_With_C-Sharp/Human/Program.cs
+++ LanguageFundamentals/OOP_With_C-Sharp/Human/Program.cs
@@ -6,7 +6,40 @@ namespace Human
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Human ben = new Human("Ben",16,18,15,100);
+            Human joe = new Human("Joe");
+            System.Console.WriteLine(ben.Attack(joe));
+        }
+    }
+    class Human
+    {
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        private int Health;
+
+        public Human(string name)
+        {
+            Name = name;
+            Strength = 3;
+            Intelligence = 3;
+            Dexterity = 3;
+            Health = 100;
+        }
+        public Human(string name, int strength, int intelligence, int dexterity, int health)
+        {
+            Name = name;
+            Strength = strength;
+            Intelligence = intelligence;
+            Dexterity = dexterity;
+            Health = health;
+        }
+
+        public int Attack(Human target)
+        {
+            target.Health -= this.Strength * 5;
+            return target.Health;
         }
     }
 }
