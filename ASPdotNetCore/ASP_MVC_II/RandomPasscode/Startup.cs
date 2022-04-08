b2fd28ac33e1aa76fca3d0276997ade39a01b519
Benjamin Cooke Williams <83699317+bcookew@users.noreply.github.com>
2022-04-08 11:13:29 -0400

    Random Passcode assignment Set up

--- ASPdotNetCore/ASP_MVC_II/RandomPasscode/Startup.cs
+++ ASPdotNetCore/ASP_MVC_II/RandomPasscode/Startup.cs
@@ -23,6 +23,8 @@ namespace RandomPasscode
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +39,8 @@ namespace RandomPasscode
                 app.UseExceptionHandler("/Home/Error");
             }
             app.UseStaticFiles();
+            app.UseMvc();
+            app.UseSession();
 
             app.UseRouting();
 
