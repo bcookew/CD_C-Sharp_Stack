8f0595572eb1d5c45d38b42adae9acfd5e4e767a
Benjamin Cooke Williams <83699317+bcookew@users.noreply.github.com>
2022-04-11 16:40:59 -0400

    CRUDelicious Complete

--- ORMs/EntityFramework/CRUDelicious/Controllers/HomeController.cs
+++ ORMs/EntityFramework/CRUDelicious/Controllers/HomeController.cs
@@ -20,28 +20,100 @@ namespace CRUDelicious.Controllers
             _context = context;
         }
 
+        // ================================
+        // ================================== Home Route
+        // ================================
         public IActionResult Index()
         {
+            ViewBag.AllDishes = _context.Dishes.ToList();
             return View();
         }
+
+        // ================================
+        // ================================== Load new dish form
+        // ================================
         [HttpGet("DishForm")]
         public IActionResult NewDish()
         {
             return View();
         }
+
+        // ================================
+        // ================================== Save new dish to DB
+        // ================================
         [HttpPost("AddNew")]
         public IActionResult AddNew(Dish dish)
         {
             if(ModelState.IsValid)
             {
+                _context.Dishes.Add(dish);
+                _context.SaveChanges();
                 return RedirectToAction("Index");
             }
             else
             {
                 return View("NewDish");
             }
-            
         }
+
+        // ================================
+        // ================================== Display Dish details by ID
+        // ================================
+        [HttpGet("dish/{id}")]
+        public IActionResult ViewDish(int id)
+        {
+            Dish QueriedDish = _context.Dishes
+                .SingleOrDefault(dish => dish.DishId == id);
+            return View("Dish", QueriedDish);
+        }
+
+        // ================================
+        // ================================== Edit Dish by Id
+        // ================================
+        [HttpGet("edit/{id}")]
+        public IActionResult Edit(int id)
+        {
+            Dish QueriedDish = _context.Dishes
+                .SingleOrDefault(dish => dish.DishId == id);
+            return View("EditDish", QueriedDish);
+        }
+
+        // ================================
+        // ================================== Update DB info
+        // ================================
+        [HttpPost("update")]
+        public IActionResult Update(Dish dish)
+        {
+            // Pull DB Copy
+            Dish QueriedDish = _context.Dishes
+                .SingleOrDefault(d => d.DishId == dish.DishId);
+            // Update Values
+            QueriedDish.Name = dish.Name;
+            QueriedDish.Chef = dish.Chef;
+            QueriedDish.Description = dish.Description;
+            QueriedDish.Calories = dish.Calories;
+            QueriedDish.Tastiness = dish.Tastiness;
+            QueriedDish.UpdatedAt = DateTime.Now;
+            // Save back to DB
+            _context.SaveChanges();
+            // Redirect to homepage
+            return View("Dish", dish);
+        }
+
+        // ================================
+        // ================================== Remove a dish by ID
+        // ================================
+        
+        [HttpGet("delete/{id}")]
+        public IActionResult RemoveDish(int id)
+        {
+            Dish QueriedDish = _context.Dishes
+                .SingleOrDefault(dish => dish.DishId == id);
+            _context.Dishes.Remove(QueriedDish);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
         public IActionResult Privacy()
         {
             return View();
