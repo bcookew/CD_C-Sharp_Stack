ce43251afccb6cf7e8a2eefb51dad526025bd5a6
Benjamin Cooke Williams <83699317+bcookew@users.noreply.github.com>
2022-04-17 16:53:45 -0400

    Message Model Created

---
+++ BeltReview/TheWall/Models/MessageModel.cs
@@ -0,0 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TheWall.Models
+{
+    public class Message
+    {
+        // ====================== Message Table Model
+        [Key]
+        public int MessageId { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        [MinLength(3, ErrorMessage ="Min 3 Chars")]
+        public string Text { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+        // ====================== Nav Properties
+        public User Author { get; set; }
+    }
+}
