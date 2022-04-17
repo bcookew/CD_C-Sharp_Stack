651ba5b7aae941cc1bc6bf99ff080b30b73a7483
Benjamin Cooke Williams <83699317+bcookew@users.noreply.github.com>
2022-04-17 17:29:13 -0400

    Created Dashboard ViewModel

---
+++ BeltReview/TheWall/Models/DashboardModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace TheWall.Models
+{
+    public class Dashboard
+    {
+        public Message Message { get; set; }
+        public List<Message> Messages { get; set; }
+    }
+}
