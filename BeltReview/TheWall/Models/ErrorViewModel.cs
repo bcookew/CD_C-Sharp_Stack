76f49c57fa93ca52e0c5cd584609e3538de6fb4b
Benjamin Cooke Williams <83699317+bcookew@users.noreply.github.com>
2022-04-17 16:41:47 -0400

    Updated starting point

--- BeltReview/TheWall/Models/ErrorViewModel.cs
+++ BeltReview/TheWall/Models/ErrorViewModel.cs
@@ -1,8 +1,11 @@
-namespace TheWall.Models;
+using System;
 
-public class ErrorViewModel
+namespace TheWall.Models
 {
-    public string? RequestId { get; set; }
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
 }
