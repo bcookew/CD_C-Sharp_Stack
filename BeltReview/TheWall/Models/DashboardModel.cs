b4e9d7ce5069156a8d5acc33394be2db5f30a96b
Benjamin Cooke Williams <83699317+bcookew@users.noreply.github.com>
2022-04-17 19:53:06 -0400

    Updated property names for greater clarity

--- BeltReview/TheWall/Models/DashboardModel.cs
+++ BeltReview/TheWall/Models/DashboardModel.cs
@@ -5,7 +5,7 @@ namespace TheWall.Models
 {
     public class Dashboard
     {
-        public Message Message { get; set; }
+        public Message NewMessage { get; set; }
         public List<Message> Messages { get; set; }
         public User User { get; set; }
     }
--- BeltReview/TheWall/Models/MessageModel.cs
+++ BeltReview/TheWall/Models/MessageModel.cs
@@ -14,8 +14,9 @@ namespace TheWall.Models
         [Required]
         public int UserId { get; set; }
         [Required]
+        [Display(Name ="Post a message")]
         [MinLength(3, ErrorMessage ="Min 3 Chars")]
-        public string Text { get; set; }
+        public string MessageText { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime UpdatedAt { get; set; } = DateTime.Now;
 
