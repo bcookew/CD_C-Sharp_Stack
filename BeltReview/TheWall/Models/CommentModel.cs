5beb7069380c9eb64ea3ea82454254ec3b9c7c11
Benjamin Cooke Williams <83699317+bcookew@users.noreply.github.com>
2022-04-17 16:56:07 -0400

    Comment Model Created

---
+++ BeltReview/TheWall/Models/CommentModel.cs
@@ -0,0 +1,26 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TheWall.Models
+{
+    public class Comment
+    {
+        // ====================== Comment Table Model
+        [Key]
+        public int CommentId { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public int MessageId { get; set; }
+        [Required]
+        [MinLength(3, ErrorMessage ="Min 3 Chars")]
+        public string Text { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+        // ====================== Nav Properties
+        public User CommentAuthor { get; set; }
+        public Message CommentedMessage { get; set; }
+    }
+}
