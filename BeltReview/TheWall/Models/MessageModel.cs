19ff93ed5d24d9b68ae316fa7586a4f997eb27af
Benjamin Cooke Williams <83699317+bcookew@users.noreply.github.com>
2022-04-17 16:57:18 -0400

    Added comment nav prop

--- BeltReview/TheWall/Models/MessageModel.cs
+++ BeltReview/TheWall/Models/MessageModel.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -20,5 +21,6 @@ namespace TheWall.Models
 
         // ====================== Nav Properties
         public User Author { get; set; }
+        public List<Comment> CommentsOnMessage { get; set; }
     }
 }
