2111f07c5db42b229dbb83c39f7dfa8850399ca0
Benjamin Cooke Williams <83699317+bcookew@users.noreply.github.com>
2022-04-18 12:02:50 -0400

    Created Profile ViewModel and Profile file

---
+++ BeltReview/DashboardApp/Models/ProfileModel.cs
@@ -0,0 +1,10 @@
+
+namespace DashboardApp.Models
+{
+    public class Profile
+    {
+        public User User { get; set; }
+        public Message Message { get; set; }
+        public Comment Comment { get; set; }
+    }
+}
